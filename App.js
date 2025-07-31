50196aaa5e136265f6be61ba60a83af632358e0c
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-07-31 17:21:13 +1000

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Zero Puffs</Text>
+      <Text>ZeroPuffs</Text>
 
       <View style={styles.CircleView}>
        <Text style={{color: "black", fontSize: 60, fontWeight: "semibold"}}>0</Text>
@@ -27,6 +27,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   PuffButton: {
+    position: "relative",
+    top: "25%",
     height: 48,
     width: "80%",
     borderRadius: 20,
@@ -39,8 +41,9 @@ const styles = StyleSheet.create({
      width: 200, // Set desired width
     height: 200, // Set desired height (must be equal to width)
     borderRadius: 100, // Half of the width/height
-    border: '3px solid',
-    backgroundColor: 'gray', // Example background color
+    borderWidth: 6,
+    borderColor: "red",
+    backgroundColor: 'transparent', // Example background color
     justifyContent: "center",
     alignItems: "center",
   }

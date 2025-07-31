c7e460ea91a76a2429686ecfcfe3d920b8a1f9c4
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-07-31 16:46:18 +1000

    Update App.js

--- App.js
+++ App.js
@@ -38,7 +38,7 @@ const styles = StyleSheet.create({
   CircleView: {
      width: 200, // Set desired width
     height: 200, // Set desired height (must be equal to width)
-    borderRadius: 50, // Half of the width/height
+    borderRadius: 100, // Half of the width/height
     border: "2px solid",
     borderColor: "gray",
     backgroundColor: 'blue', // Example background color

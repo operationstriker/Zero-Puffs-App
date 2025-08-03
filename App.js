a5590d9f88896db3c98b9610045152e28bdbf620
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-03 11:46:35 +1000

    Update App.js

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: "35"}}>Zero Puffs</Text>
+      <Text style={{fontSize: "35", position: "relative"}}>Zero Puffs</Text>
       
 
 

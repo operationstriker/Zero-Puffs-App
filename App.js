1112ba2f592b829e4681a73ddaa62b485b8efef1
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-03 22:00:44 +1000

    Update App.js

--- App.js
+++ App.js
@@ -30,7 +30,6 @@ export default function App() {
 
       {/* images, plus, graph, settings */}
        
-       <img src={PlusImage}></img>
 
 
 
@@ -70,6 +69,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   CircleView: {
+    position: "relative",
      width: 200, // Set desired width
     height: 200, // Set desired height (must be equal to width)
     borderRadius: 100, // Half of the width/height
@@ -80,6 +80,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
   }
 });
+
+
+// start expo
+//npx expo start
+
 
 
 // start expo

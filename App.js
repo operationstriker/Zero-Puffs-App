8bd18c0989615120580458a04033690edc99de22
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-03 22:11:22 +1000

    Update App.js

--- App.js
+++ App.js
@@ -76,7 +76,7 @@ const styles = StyleSheet.create({
   },
   CircleView: {
     position: "relative",
-    bottom: "20%",
+    bottom: "15%",
      width: 200, // Set desired width
     height: 200, // Set desired height (must be equal to width)
     borderRadius: 100, // Half of the width/height

3d761002456781ea3c6d328ffe5d00f8eaf00bf8
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-17 16:06:12 +1000

    Update Home2.js

---
+++ Screens/Home2.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
+import React, { useState } from "react"
+import { Tabs } from "expo-router"
+
+
+export default function App() {
+
+  const [Count, setCount] = useState(0)
+
+
+  return (
+    <View style={styles.container}>
+     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Header: {
+    position: "relative",
+    right: 85,
+    height: 50,
+    flexDirection: "row",
+    width: 130,
+    marginRight: 20
+  }
+
+});
+

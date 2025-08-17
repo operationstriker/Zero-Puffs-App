e4b7558e8e7d5de2e34ff392306c926dac25d5ee
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-17 14:12:16 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
-import { LayoutScreen } from './Screens/Layout.js';
+import App2 from './Screens/Home.js'
+// import { LayoutScreen } from './Screens/Layout.js';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
-registerRootComponent(LayoutScreen);
+registerRootComponent(App2);
+// registerRootComponent(LayoutScreen);

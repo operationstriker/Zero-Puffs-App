32986271fc18cec6e3a988a8979d1113d484301d
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-17 11:19:54 +1000

    Update Home.js

---
+++ Home.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
+import React, { useState } from "react"
+import { Tabs } from "expo-router"
+
+
+export default function App() {
+
+  const [Count, setCount] = useState(0)
+
+
+  return (
+    <View style={styles.container}>
+            <TouchableOpacity style={styles.PuffButton2}>
+            <Text style={{textAlign: "center", color: "red", fontSize: "28", fontWeight: "semibold"}}>Yesterday</Text>
+          </TouchableOpacity>
+
+
+       <Text>Zero Puffs</Text>
+
+       <Text>Daily Total</Text>
+       <Text>Daily Average</Text>
+
+        <Text>Lifetime Puffs</Text>
+       <Text>Nicotine Intake</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});

b47131a6e34f49b85e2d0883641cc085bfbff836
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-17 11:23:33 +1000

    Update Home.js

--- Home.js
+++ Home.js
@@ -11,7 +11,9 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-        <View></View>
+        <View>
+            
+        </View>
             <TouchableOpacity style={styles.PuffButton2}>
             <Text>Yesterday</Text>
           </TouchableOpacity>

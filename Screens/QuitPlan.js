d7abdddf737b2c5fce069c21085efd84bb322cb3
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-18 05:21:40 +1000

    Update QuitPlan.js

--- Screens/QuitPlan.js
+++ Screens/QuitPlan.js
@@ -11,7 +11,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-     <Text style={{fontWeight: "bold", fontSize: 20, textAlign: "center", position: "relative", bottom: "20%"}}>Quit Plan</Text>
+     <Text style={{fontWeight: "bold", fontSize: 20, textAlign: "center", position: "relative", bottom: "40%"}}>Quit Plan</Text>
     </View>
   );
 }

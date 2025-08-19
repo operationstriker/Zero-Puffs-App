7faeed9b189ae7d8253f261d9bcb94d7235f6f42
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-20 05:21:53 +1000

    Update Layout.js

--- Screens/Layout.js
+++ Screens/Layout.js
@@ -1,8 +1,18 @@
 import { Tabs } from 'expo-router';
+import { Stack } from 'expo-router'
 
 export default function TabLayout() {
   return (
     <Tabs>
+    <Tabs.Screen name='Quit' options={{
+      tabBarLabel: "Home"
+    }}/>
+    <Tabs.Screen name='Puffs' options={{
+      tabBarLabel: "Home"
+    }}/>
+    <Tabs.Screen name='Profile' options={{
+      tabBarLabel: "Home"
+    }}/>
     </Tabs>
   );
 }

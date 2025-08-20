dfd6db66a624956526946059d7fc36704ace7818
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-20 10:02:17 +1000

    Update Home2.js

--- Home2.js
+++ Home2.js
@@ -1,17 +1,81 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
 import React, { useState } from "react"
-import { Tabs } from "expo-router"
-
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
+import { TextInput } from 'react-native';
 
 export default function App() {
 
-  const [Count, setCount] = useState(0)
+    const [value, setValue] = useState(0) // create a variable
+    const maxValue = 20;
+    const fill = (value / maxValue) * 100;
+
+  function pufffunc() {
+    setValue(value + 1)
+  }
+
+  const [isVisible, setVisibility] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisibility(!isVisible);
+  }
+
+  const CloseDiv = () => {
+    setVisibility(false)
+  }
 
 
   return (
     <View style={styles.container}>
-     
+       <Text style={{fontSize: 30, fontFamily: "Calbri", bottom: "13%", color: "#1E1E1E", textAlign: "center"}}>ZERO PUFFS </Text>
+
+        <AnimatedCircularProgress
+        size={220}
+        width={17}
+        fill={fill}
+        tintColor="#ff0000ff"
+        backgroundColor="#bababaff"
+          rotation={-0}>
+        {
+          (fill) => (
+            <View>
+             <Text style={{fontSize: 40, textAlign: "center"}}> {value}</Text>
+             <Text style={{fontSize: 20}}>out of 25</Text>
+            </View>
+            
+          )
+        }
+      </AnimatedCircularProgress>
+
+      <View style={{position: "relative", top: "7%", left: "5%"}}>
+        <View style={styles.vstack}>
+        <Text style={{marginBottom: 20}}>20 </Text>
+        <Text style={{marginBottom: 20}}>15 </Text>
+        <Text style={{marginBottom: 20}}>10 </Text>
+        <Text style={{marginBottom: 20}}>5</Text>
+        </View>
+
+      <View style={styles.hstack}>
+        <Text style={{marginRight: 35}}>0</Text>
+        <Text style={{marginRight: 35}}>5</Text>
+        <Text style={{marginRight: 35}}>10</Text>
+        <Text style={{marginRight: 35}}>15</Text>
+        <Text style={{marginRight: 35}}>20</Text>
+        <Text style={{marginRight: 35}}>25</Text>
+        </View>
+      </View>
+
+        <TouchableOpacity style={styles.PuffButton2} onPress={toggleVisibility}>
+        <Text style={{textAlign: "center", color: "red", fontSize: "28", fontWeight: "semibold"}}>UPDATE  PUFFS</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.PuffButton} onPress={(() => setValue(value + 1))}>
+        <Text style={{textAlign: "center", color: "white", fontSize: "28", fontWeight: "semibold"}} onPress={pufffunc}>PUFF</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+
+
     </View>
   );
 }
@@ -23,13 +87,41 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  Header: {
+  PuffButton: {
     position: "relative",
-    right: 85,
-    height: 50,
-    flexDirection: "row",
-    width: 130,
-    marginRight: 20
+    top: "16%",
+    height: 55,
+    width: "85%",
+    borderRadius: 27,
+    backgroundColor: "#FF0000",
+    cursor: "pointer",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  PuffButton2: {
+        position: "relative",
+    top: "15%",
+    height: 55,
+    width: "85%",
+    borderWidth: 2,
+    borderColor: "red",
+    borderRadius: 27,
+    backgroundColor: "white",
+    cursor: "pointer",
+    justifyContent: "center",
+    alignItems: "center",
+    boxshadow: "0 0 0 5px rgba(255, 10, 10, 1);"
+  },
+  vstack: {
+    flexDirection: "column"
+  },
+  hstack: {
+    flexDirection: "row"
+  },
+  UpdateDivElement: {
+    backgroundColor: "rgba(220, 220, 220, 1)",
+    borderRadius: 20,
+    height: 240,
+    width: 280
   }
-
 });

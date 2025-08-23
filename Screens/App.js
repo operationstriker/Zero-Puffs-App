d17fe00f56b38afe8b9a868eb9f569617a30a58d
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-23 19:37:45 +1000

    Update App.js

--- Screens/App.js
+++ Screens/App.js
@@ -1,1 +1,22 @@
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image, Label } from 'react-native';
+import React, { useState } from "react"
+import { Link } from "expo-router"
 
+export default function App() {
+
+
+  return (
+    <View style={styles.container}>
+     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});

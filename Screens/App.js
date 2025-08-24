81fe2a18c18d6946e60ee4454b8f057ff8818f7b
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-24 14:13:22 +1000

    Update App.js

--- Screens/App.js
+++ Screens/App.js
@@ -1,18 +1,13 @@
 import { StyleSheet, Text, View, Button, TouchableOpacity, Image, Label } from 'react-native';
-import React from "react"
-import { StatusBar } from 'expo-status-bar';
-import { Tabs } from "expo-router"
+import React, { useState } from "react"
+import { Link } from "expo-router"
 
 export default function App() {
 
 
   return (
-   <React.Fragment>
-    <StatusBar style='auto'/>
-    <Tabs>
-     <Tabs.Screen name='index' />
-    </Tabs>
-   </React.Fragment>
+    <View style={styles.container}>
+    </View>
   );
 }
 

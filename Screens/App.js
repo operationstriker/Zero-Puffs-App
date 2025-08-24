881afe12125378c0a45752b2ef1a5568c844ecad
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-24 14:44:26 +1000

    Update App.js

--- Screens/App.js
+++ Screens/App.js
@@ -1,6 +1,5 @@
 import { StyleSheet, Text, View, Button, TouchableOpacity, Image, Label } from 'react-native';
 import React, { useState } from "react"
-import { Link } from "expo-router"
 import { Tabs } from "expo-router"
 
 export default function App() {

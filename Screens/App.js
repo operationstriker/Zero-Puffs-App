242e4ed3129984e1262709e94c093acf048ad8da
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-24 15:51:08 +1000

    Update App.js

--- Screens/App.js
+++ Screens/App.js
@@ -1,6 +1,5 @@
 import { StyleSheet, Text, View, Button, TouchableOpacity, Image, Label } from 'react-native';
 import React, { useState } from "react"
-import { Link } from "expo-router"
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 

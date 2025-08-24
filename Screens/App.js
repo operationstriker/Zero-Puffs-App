b3dbe21fb1db79fa610e8e6cb0df77a674486a4e
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-24 15:57:35 +1000

    Update App.js

--- Screens/App.js
+++ Screens/App.js
@@ -3,13 +3,17 @@ import React, { useState } from "react"
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+function HomeScreen() {
+
+}
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home2}>
-       <Tab.Screen/>
+      <Tab.Screen name="Home" component={HomeScreen}>
       </Tab.Navigator>
     </NavigationContainer>
   );

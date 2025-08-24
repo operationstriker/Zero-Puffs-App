358c1389bc7cbafb42ba8b28e41eeb1ea19d4f36
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-25 00:53:48 +1000

    Update Tracker.js

--- Screens/Tracker.js
+++ Screens/Tracker.js
@@ -1,86 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TouchableOpacity, Image, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image, Label } from 'react-native';
 import React, { useState } from "react"
-import { Tabs } from "expo-router"
-// Vape Usage
-import { AnimatedCircularProgress } from 'react-native-circular-progress';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from "./Home"
+import Quit from "./QuitPlan"
+import Tracker from "./Tracker"
 
-
-export default function App() {
-
-  const [value, setValue] = useState(1) // create a variable
-  const maxValue = 20;
-
-  const fill = (value / maxValue) * 100;
-
-
-  return (
-    <View style={styles.container}>
-      <Text style={{fontSize: 30, fontFamily: "Calbri", bottom: "21%", color: "#1E1E1E", textAlign: "center", margin: 0}}>ZERO PUFFS</Text>
-      <Text style={{fontSize: 20, bottom: "21%"}}>PUFF Tracker</Text>
-
-
-        <View style={styles.Header}>
-            <TouchableOpacity style={styles.PuffButton2}>
-            <Text style={{textAlign: "center", fontSize: "20", fontWeight: "semibold", marginRight: 20}}>Yesterday</Text>
-          </TouchableOpacity>
-
-        <TouchableOpacity style={styles.PuffButton2}>
-        <Text style={{textAlign: "center", fontSize: "20", fontWeight: "semibold", marginRight: 20}}>Week</Text>
-          </TouchableOpacity>
-
-        <TouchableOpacity style={styles.PuffButton2}>
-        <Text style={{textAlign: "center", fontSize: "20", fontWeight: "semibold", marginRight: 20}}>Month</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.PuffButton2}>
-        <Text style={{textAlign: "center", fontSize: "20", fontWeight: "semibold"}}>All Time</Text>
-        </TouchableOpacity>
-        </View>
-
-       <View style={{top: 100}}>
-       <View style={{top: 160, right: 60}}>
-       <Text style={{fontSize: 30}}>Daily Total</Text>
-       <Text style={{textAlign: "center", fontSize: 20, right: 27}}>0</Text>
-       </View>
-       
-       <View style={{top: 100, left: 90}}>
-      <Text style={{textAlign: "center", fontSize: 30}}>Daily Average</Text>
-      <Text style={{textAlign: "center", fontSize: 20}}>1</Text>
-       </View>
-       
-       
-       <View style={{top: 120, right: 86}}>
-        <Text style={{textAlign: "center", fontSize: 30}}>Total Puffs</Text>
-        <Text style={{textAlign: "center", fontSize: 20}}>0</Text>
-       </View>
-
-       <View style={{top: 60, left: 94}}>
-       <Text style={{textAlign: "center", fontSize: 30}}>Nicotine Intake</Text>
-       <Text style={{textAlign: "center", fontSize: 20}}>0</Text>
-       </View>
-      </View>
-       
-
-    </View>
-  );
+function HomeScreen() {
+   return (
+    <Home />
+   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  Header: {
-    position: "relative",
-    right: 85,
-    height: 50,
-    bottom: 170,
-    flexDirection: "row",
-    width: 130,
-    marginRight: 20
-  },
+function QuitScreen() {
+   return (
+    <Quit />
+   )
+}
 
-});
+function TrackerScreen() {
+   return (
+    <Tracker />
+   )
+}
+
+
+const Tab = createBottomTabNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{
+        tabBarLabelStyle: {
+          // borderTopWidth: 0,
+          fontSize: 16,
+          borderTopWidth: 0,
+          elevation: 0, 
+      
+        }
+      }}>
+      <Tab.Screen name="Quit" component={QuitScreen} options={{headerShown: false}}/>
+      <Tab.Screen name="Puff" component={HomeScreen} options={{headerShown: false}}/>
+      <Tab.Screen name="Tracker" component={TrackerScreen} options={{headerShown: false}}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}

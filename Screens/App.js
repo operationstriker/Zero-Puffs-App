9fc237bf77c263fca0dd319c1d777890a92f643a
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-24 15:50:43 +1000

    Update App.js

--- Screens/App.js
+++ Screens/App.js
@@ -4,13 +4,13 @@ import { Link } from "expo-router"
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+const Tab = createBottomTabNavigator()
+
 export default function App() {
-
-
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        
+
       </Tab.Navigator>
     </NavigationContainer>
   );

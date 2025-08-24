36ccda3836542c9777d30c890f9b87b9f6aacf5e
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-08-24 15:54:46 +1000

    Update App.js

--- Screens/App.js
+++ Screens/App.js
@@ -3,7 +3,6 @@ import React, { useState } from "react"
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
